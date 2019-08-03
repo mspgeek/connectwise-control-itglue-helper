3c469d211fca7fae4c6dcd0e63e9319e40c4d0a3
Kevin Grube <grube.kevin@gmail.com>
2019-08-03 04:23:48 -0700

    Create redux/search

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,10 +4,12 @@ import auth from './auth';
 import alert from './alert';
 import organizations from './organizations';
 import passwords from './passwords';
+import search from './search';
 
 export default combineReducers({
   auth,
   alert,
   organizations,
   passwords,
+  search,
 });
